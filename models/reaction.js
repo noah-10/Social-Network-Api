4e1704a3428b49b61d15ea421ce729829d454a99
noah-10 <noah.fryman.10@gmail.com>
2024-03-25 23:16:23 -0400

    Added models

---
+++ models/reaction.js
@@ -0,0 +1,28 @@
+const { Schema, Types, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            get: function (date) {
+                return date.toDateString();
+            },
+        },
+    },
+);
+
+module.exports = reactionSchema;
---
+++ models/thought.js
@@ -0,0 +1,39 @@
+const { Schema, Types, model } = require("mongoose");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      min: 1,
+      max: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get: function (date) {
+        return date.toDateString();
+      },
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
