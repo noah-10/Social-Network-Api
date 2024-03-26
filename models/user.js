27286f9c795805f48b56ede9dbf7d8495cf1d322
noah-10 <noah.fryman.10@gmail.com>
2024-03-25 22:52:13 -0400

    Added folder and file architecture

---
+++ models/user.js
@@ -0,0 +1,50 @@
+const { Schema, Types, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trimmed: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: {
+                validator: function(v) {
+                    return /^([a-zA-Z0-9_\.-]+)@([\da-z\.]+)\.([\w-]{2,6})$/.test(v);
+                },
+                message: props => `${props.value} is not a valid email!`
+            },
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought'
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false,
+    }
+);
+
+userSchema.virtual('firendCount')
+.get(function () {
+    return this.friends.length;
+})
+
+const User = model('user', userSchema);
+
+module.exports = User;
